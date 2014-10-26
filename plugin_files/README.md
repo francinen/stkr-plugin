100e7ca65eb445ebccf37dc344eb1b9406874e9f
Francine N <hello@francine.codes>
2014-10-26 11:58:55 -0400

    consolidated all relevant plugin files into a single folder

---
+++ plugin_files/README.md
@@ -0,0 +1,89 @@
+# Stkr
+
+## Plugin Overview
+
+An easy way to control when and where fixed elements appear on your web page.
+
+### View Demo &#9654; 
+
+## Getting Started
+1\. Download and open **stkr-plugin.zip**.
+
+2\. Transfer **stkr.min.js** from the **plugin_files** folder to your project folder.
+
+3\. Load the **jQuery CDN** and **stkr.min.js** into your HTML file using the `<script>` tag.
+
+4\. Give the desired "sticky" element an `id`. If you're including multiple sticky elements that will become fixed at different points on the web page, they should each have a distinct ID name.
+
+```
+<img href="images/parachute.png" id="sticky1">
+<img href="images/cloud.png" id="sticky2">
+<img href="images/bird.png" id="sticky3">
+``` 
+
+5\. Call the stkr method, `.stkr()`, in your JavaScript file. If there are multiple sticky elements on the page, invoke a new jQuery function for each.
+
+```
+$('#sticky1').stkr();
+$('#sticky2').stkr();
+$('#sticky3').stkr();
+```
+
+## Default Settings
+By default, the sticky element remains fixed at the top-left corner for the entire length of your web page, with its `top` and `left` properties set to 20px.
+
+```
+position: fixed,
+top: 20px,
+left: 20px
+```
+
+## Customizable Settings
+
+### Set custom start and end points
+**startSticky:** A `string` identifying the name, ID, or class of an element. When the user scrolls to this point, the sticky element becomes fixed on the page. If using an ID as the selector, include the hashtag in the string (e.g., `'#idName'`). If using a class name, include the dot (e.g., `'.className'`) in the string. Default value: `'body'`.
+***
+**endSticky:** A `string` identifying the name, ID, or class of an element. This marks the end of the fixed element's designated "sticky zone". If this is not set, the element will remain fixed for the remaining length of the document. If using an ID as the selector, include the hashtag in the string (e.g., `'#idName'`). If using a class name, include the dot (e.g., `'.className'`) in the string. Default value: `'null'`.
+***
+**offsetStick:** An `integer` that offsets the point at which the element becomes fixed on the page. 
+
+**offsetUnstick:** An `integer` that offsets the point at which the element is no longer fixed on the page.
+
+### Adjust position of fixed element(s)
+**stickyPosition:** A `string` that determines where the fixed element is positioned on the page. Accepted values: `'top-left`, `'top-right`,`'bottom-left`,`'bottom-right`,`'custom'`
+***
+**top, right, bottom, left:** If `stickyPosition` is set to `'custom'`, adjust the element's `top`, `right`, `bottom`, and/or `left` properties to place it in the desired position. Accepted values: `Integer` if using pixels (e.g., `30`) or `string` if using percentages (e.g., `'30%'`).
+
+```
+$('#sticky-profile').stkr({
+stickyPosition: 'custom',
+top: '50%',
+left: 40
+});
+```
+
+### Extras
+
+**toggleVisibility:** Set to `false` by default. Setting this to `true` will make the element disappear from the page when it is not between its designated start and end points. If changed to `true`, make sure to set the element's `visibility` to `hidden` in its CSS declaration.
+
+```
+#sticky {
+visibility: hidden
+}
+```
+***
+**toggleFade:** Applies a fade in/fade out effect on the fixed element as it enters or leaves its designated sticky zone. Set to `false` by default. If changed to `true`, set the element's `opacity` to `0` and include the `transition` property in its respective CSS declaration.
+
+```
+#sticky {
+opacity: 0;
+-webkit-transition: opacity 0.2s;
+transition: opacity 0.2s;
+}
+```
+
+***
+**horizontal:** Set to `false` by default for vertical scrolling. Change to `true` if `.stkr()` will be triggered by a horizontal scroll.
+
+## Contribute
+### Fork stkr and help make it better!
---
+++ plugin_files/stkr.js
@@ -0,0 +1,210 @@
+$.fn.stkr = function(options){
+	var $sticky = $(this);
+	var originalPosition = $(this).css('position');
+
+	var settings = $.extend({
+		startSticky: 'body',
+		endSticky: null,
+		stickyPosition: 'top-left',
+		top: null,
+		left: null,
+		bottom: null,
+		right: null,
+		offsetStick: 0,
+		offsetUnstick: 0,
+		horizontal: false,
+		toggleVisibility: false,
+		toggleFade: false
+	}, options);
+
+	var checkStickSettings = function(){
+		if (settings.stickyPosition=='top-left'){
+			if (settings.toggleFade){
+				return $sticky.css({
+				position: 'fixed',
+				top: 20, 
+				left: 20,
+				opacity: 1
+				});
+			};
+			if (settings.toggleVisibility){
+				return $sticky.css({
+				position: 'fixed',
+				top: 20, 
+				left: 20,
+				visibility: 'visible',
+				});
+			};
+			if(!settings.toggleFade && !settings.toggleVisibility){
+				return $sticky.css({
+				position: 'fixed',
+				top: 20, 
+				left: 20
+				});
+			};
+		}else if(settings.stickyPosition=='top-right'){
+			if (settings.toggleFade){
+				return $sticky.css({
+				position: 'fixed',
+				top: 20, 
+				right: 20,
+				opacity: 1
+				});
+			};
+			if (settings.toggleVisibility){
+				return $sticky.css({
+				position: 'fixed',
+				top: 20, 
+				right: 20,
+				visibility: 'visible',
+				});
+			};
+			if(!settings.toggleFade && !settings.toggleVisibility){
+				return $sticky.css({
+				position: 'fixed',
+				top: 20, 
+				right: 20
+				});
+			};
+		}else if(settings.stickyPosition=='bottom-right'){
+			if (settings.toggleFade){
+				return $sticky.css({
+				position: 'fixed',
+				bottom: 20, 
+				right: 20,
+				opacity: 1
+				});
+			};
+			if (settings.toggleVisibility){
+				return $sticky.css({
+				position: 'fixed',
+				bottom: 20, 
+				right: 20,
+				visibility: 'visible',
+				});
+			};
+			if(!settings.toggleFade && !settings.toggleVisibility){
+				return $sticky.css({
+				position: 'fixed',
+				bottom: 20, 
+				right: 20
+				});
+			};
+		}else if (settings.stickyPosition=='bottom-left'){
+			if (settings.toggleFade){
+				return $sticky.css({
+				position: 'fixed',
+				bottom: 20, 
+				left: 20,
+				opacity: 1
+				});
+			};
+			if (settings.toggleVisibility){
+				return $sticky.css({
+				position: 'fixed',
+				bottom: 20, 
+				left: 20,
+				visibility: 'visible',
+				});
+			};
+			if(!settings.toggleFade && !settings.toggleVisibility){
+				return $sticky.css({
+				position: 'fixed',
+				bottom: 20, 
+				left: 20
+				});
+			};
+		}else{
+			if (settings.toggleFade){
+				return $sticky.css({
+					position: 'fixed',
+					top: settings.top,
+					left: settings.left,
+					bottom: settings.bottom,
+					right: settings.right,
+					opacity: 1
+				});
+			};
+			if (settings.toggleVisibility){
+				return $sticky.css({
+					position: 'fixed',
+					top: settings.top,
+					left: settings.left,
+					bottom: settings.bottom,
+					right: settings.right,
+					visibility: 'visible'
+				});
+			};
+			if(!settings.toggleFade && !settings.toggleVisibility){
+				return $sticky.css({
+				position: 'fixed',
+				top: settings.top,
+				left: settings.left,
+				bottom: settings.bottom,
+				right: settings.right
+				});
+			}
+		};
+	};
+
+	var checkUnstickSettings = function(){
+		if (settings.toggleFade){
+			if (!originalPosition){
+				return $sticky.css({position:'static', opacity: 0});
+			}else{
+				return $sticky.css({position: originalPosition, opacity: 0});
+			}
+		};
+		if (settings.toggleVisibility){
+			if (!originalPosition){
+				return $sticky.css({position:'static', visibility: 'hidden'});
+			}else{
+				return $sticky.css({position: originalPosition, visibility: 'hidden'});
+			}
+		};
+		if(!settings.toggleFade && !settings.toggleVisibility){
+			if (!originalPosition){
+				return $sticky.css({position:'static'});
+			}else{
+				return $sticky.css({position: originalPosition});
+			}						
+		}
+	};
+
+	$(document).scroll(function(){
+		var x = $(settings.startSticky).position();
+		if (!settings.horizontal){
+			var y = $(document).scrollTop();
+			if (!settings.endSticky){
+				if (y > (x.top-settings.offsetStick)) {
+					checkStickSettings();
+				}
+			}else{
+				var z = $(settings.endSticky).position();
+				if (y < (x.top-settings.offsetStick)){
+					checkUnstickSettings();
+				}else if (y > (x.top-settings.offsetStick) && y < (z.top+settings.offsetUnstick)) {
+						checkStickSettings();
+				}else{
+					checkUnstickSettings();
+				}
+			}
+		}else{
+			var y = $(document).scrollLeft();
+			if (!settings.endSticky){
+				if (y > (x.left-settings.offsetStick)) {
+					checkStickSettings();
+				}else{
+					checkUnstickSettings();
+				}
+			}else{
+				var z = $(settings.endSticky).position();
+				if (y > (x.left-settings.offsetStick) && y < (z.left-settings.offsetUnstick)) {
+					checkStickSettings();
+				}else{
+					checkUnstickSettings();
+				}
+			}
+		}
+	});	
+};
